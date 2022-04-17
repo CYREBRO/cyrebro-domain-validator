17e5105117850e0f586a86839d82df57a39f3ecd
CYREBRO-Innovation <innovation@cyrebro.io>
2022-04-17 10:45:21 +0300

    Usage Updates
    Updates to the package usages that makes it easier.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyrebro_domain_validator',
-    version='0.2.0',
+    version='0.2.2',
     author='CYREBRO Innovation',
     author_email='innovation@cyrebro.io',
     description='A domain validation package, written by CYREBRO\'s Innovation team.',
